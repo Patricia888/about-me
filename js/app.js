a6b3ea0e7d38d28710b1eb007b66417b626d1f2a
Patricia G Raftery <praftery8@gmail.com>
2018-01-09 14:44:05 -0800

    Added extra question in index, added first JS prompt and alert

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
+
 //What is your name prompt
 
+var userName = prompt('Hi, what is your name?');
+
 //Prompt saying get ready to play (alert)
+
+alert('Nice to meet you ' + userName + '. Get ready to play a guessing game about what I love most: Me!');
 
 //Six Y/N questions (prompts)
 //1
@@ -11,5 +16,7 @@
 //4
 //5
 //6
+
+
 
 //I am glad we are getting to know each other (alert)
