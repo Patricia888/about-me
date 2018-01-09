244a8858b7112bff272b18413c92d48510de4ae2
Patricia G Raftery <praftery8@gmail.com>
2018-01-09 14:22:17 -0800

    Fleshed out html page and wrote pseudocode for js page

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//What is your name prompt
+
+//Prompt saying get ready to play (alert)
+
+//Six Y/N questions (prompts)
+//1
+//2
+//3
+//4
+//5
+//6
+
+//I am glad we are getting to know each other (alert)
