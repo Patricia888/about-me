af1c5b1b968b9172fb2d3d93475656e8df325d4f
Patricia G Raftery <praftery8@gmail.com>
2018-01-10 16:33:03 -0800

    completed Q7 for loop

--- js/app.js
+++ js/app.js
@@ -18,11 +18,11 @@ var countries = prompt('I have been to more than 5 different countries.').toUppe
 console.log('5 countries: ' + countries);
 
 if(countries === 'Y' || countries === 'YES') {
-    alert('I have only been to 3 countries, but I would love to travel more.');
+  alert('I have only been to 3 countries, but I would love to travel more.');
 }   else if (countries === 'N' || countries === 'NO') {
-    alert('That is correct, I have only been to 3 different countries.');
+  alert('That is correct, I have only been to 3 different countries.');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 //2
@@ -30,11 +30,11 @@ var languages = prompt('I speak 4 languages').toUpperCase();
 console.log('4 languages: ' + languages);
 
 if(languages === 'Y' || languages === 'YES') {
-    alert('Correct! I speak English, Spanish, Mandarin, and ASL. I can\'t wait until I can add JavaScript to the list.');
+  alert('Correct! I speak English, Spanish, Mandarin, and ASL. I can\'t wait until I can add JavaScript to the list.');
 }   else if (languages === 'N' || languages === 'NO') {
-    alert('Nope, I do speak four. Soon to be 5 once I have mastered JavaScript!');
+  alert('Nope, I do speak four. Soon to be 5 once I have mastered JavaScript!');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 //3
@@ -42,11 +42,11 @@ var bubbleTea = prompt('I love bubble tea').toUpperCase();
 console.log('Bubble tea: ' + bubbleTea);
 
 if(bubbleTea === 'Y' || bubbleTea === 'YES') {
-    alert('True. One of my favorite indulgences.');
+  alert('True. One of my favorite indulgences.');
 }   else if (bubbleTea === 'N' || bubbleTea === 'NO') {
-    alert('Bubble tea is awesome! I love all the different types.');
+  alert('Bubble tea is awesome! I love all the different types.');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 //4
@@ -54,11 +54,11 @@ var reallyLoveBubbleTea = prompt('I really, really love bubble tea').toUpperCase
 console.log('Really love bubble tea: ' + reallyLoveBubbleTea);
 
 if(reallyLoveBubbleTea === 'Y' || reallyLoveBubbleTea === 'YES') {
-    alert('Definitely true. I once walked 3 hours to get to the nearest bubble tea store.');
+  alert('Definitely true. I once walked 3 hours to get to the nearest bubble tea store.');
 }   else if (reallyLoveBubbleTea === 'N' || reallyLoveBubbleTea === 'NO') {
-    alert('.... I am obviously not speaking to a fellow bubble tea lover.');
+  alert('.... I am obviously not speaking to a fellow bubble tea lover.');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 //5
@@ -66,11 +66,11 @@ var scarySnakes = prompt('I hate snakes').toUpperCase();
 console.log('Snakes scary: ' + scarySnakes);
 
 if(scarySnakes === 'Y' || scarySnakes === 'YES') {
-    alert('I am actually not a snake hater. Some of them can even be quite cute.');
+  alert('I am actually not a snake hater. Some of them can even be quite cute.');
 }   else if (scarySnakes === 'N' || scarySnakes === 'NO') {
-    alert('I agree, snakes are not bad!');
+  alert('I agree, snakes are not bad!');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 //6
@@ -78,32 +78,37 @@ var loveCoding = prompt('I love coding almost as much as bubble tea').toUpperCas
 console.log('Love coding: ' + loveCoding);
 
 if(loveCoding === 'Y' || loveCoding === 'YES') {
-    alert('Yes, I am having fun so far.');
+  alert('Yes, I am having fun so far.');
 }   else if (loveCoding === 'N' || loveCoding === 'NO') {
-    alert('Really? You must be pulling my leg.');
+  alert('Really? You must be pulling my leg.');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 //7 fav num
-var favNumber = prompt('What is my favorite number between 1 and 20? Respond with a number.');
-console.log('Fav Number: ' + favNumber);
-
-parseInt(favNumber);
-
 var answerFavNum = 8;
 
-for (var numTries = 0; numTries<4; numTries++) {
-    console.log('Fav Num tries: ' + favNumber);
+var favNumber;
 
-    if (favNumber === answerFavNum) {
-        alert('That is correct!!!!');
-        break;
-}   else if (favNumber > answerFavnum) {
-        alert('Too high.');
-}   else if (favNumber < answerFavNum) {
-        alert('Too low.');
-}   
+for (var numTries = 0; numTries < 4; numTries++) {
+  favNumber = prompt('What is my favorite number between 1 and 20? Respond with a number.');
+
+  favNumber = parseInt(favNumber);
+
+  console.log('Fav Num tried: ' + favNumber);
+  if (favNumber === answerFavNum) {
+    alert('That is correct!!!!');
+    break;
+  } else if (favNumber > answerFavNum) {
+    alert('Too high.');
+  } else if (favNumber < answerFavNum) {
+    alert('Too low.');
+  } else {
+    alert('Please guess a number!!');
+  }
+}
+if (numTries === 4) {
+  alert('Out of tries, better luck next time! The answer was 8.');
 }
 
 //8 fav colors
@@ -111,11 +116,11 @@ var favColors = prompt('I have 3 favorite colors. Can you guess one of them? Res
 console.log('Love coding: ' + favColors);
 
 if(favColors === 'Y' || favColors === 'YES') {
-    alert('Yes, I am having fun so far.');
+  alert('Yes, I am having fun so far.');
 }   else if (favColors === 'N' || favColors === 'NO') {
-    alert('Really? You must be pulling my leg.');
+  alert('Really? You must be pulling my leg.');
 }   else {
-    alert('Please enter Y or N!');
+  alert('Please enter Y or N!');
 }
 
 
